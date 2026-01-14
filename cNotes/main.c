2546f2dd7407f134d1d5ea44af7cd8706128504f
AustinDevoreUWF <ard78@students.uwf.edu>
2026-01-14 12:42:07 -0600

    Added Git Ignore

--- cNotes/main.c
+++
@@ -1,9 +0,0 @@
-//need to ship as an exe
-//need to open up as an app with an empty note, with some option for previous notes
-//save new files to a folder
-//close
-#include <stdio.h>
-int main(){
-printf("Hello Wrld!\n");
-return 0;
-}
