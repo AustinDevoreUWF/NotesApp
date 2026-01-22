36d560be21e73110132577ac8527aafaf0f3f455
AustinDevoreUWF <ard78@students.uwf.edu>
2026-01-21 19:58:51 -0600

    Added ability to use as a command line tool alone

--- build/CMakeFiles/CnoteMaker.dir/main.c.obj.d
+++ build/CMakeFiles/CnoteMaker.dir/main.c.obj.d
@@ -20,4 +20,13 @@ CMakeFiles/CnoteMaker.dir/main.c.obj: \
  C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
  C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/malloc.h \
  C:/Program\ Files/mingw64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mm_malloc.h \
- C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/errno.h
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/errno.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/string.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/direct.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/io.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/time.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/sys/timeb.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/sec_api/sys/timeb_s.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/_timeval.h \
+ C:/Program\ Files/mingw64/x86_64-w64-mingw32/include/pthread_time.h
--- build/bloodNotes.md
+++
@@ -1,5 +0,0 @@
-# This is the header of Blood
-## This is a subheader
-----
-Here is the *Boldened* text i think
-now im done
